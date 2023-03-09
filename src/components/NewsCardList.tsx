81037117f946584fc8f1956672b729425750bd85
Ирина Черняева <iridache@Irinas-MacBook-Air.local>
2023-03-09 17:17:18 +0300

    refresh btn added

--- src/components/NewsCardList.tsx
+++ src/components/NewsCardList.tsx
@@ -1,31 +1,44 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { restApiService } from "../utils/RestApiService";
 import { NewsItem } from "../utils/types";
 import { NewsCard } from "./NewsCard";
-import styles from "./NewsCardList.module.css"
+import styles from "./NewsCardList.module.css";
 
 export function NewsCardList() {
   const [latestNews, setLatestNews] = useState<NewsItem[]>();
+  const [isRefresh, setIsRefresh] = useState<boolean>(false);
 
   useEffect(() => {
-    restApiService.geNewsItems().then(data => setLatestNews(data))
-    }, [])
+    restApiService.geNewsItems().then((data) => setLatestNews(data));
+
+    const interval = setInterval(() => {
+      restApiService.geNewsItems().then((data) => setLatestNews(data));
+    }, 60000);
+    return () => clearInterval(interval);
+  }, [isRefresh]);
 
   return (
     <>
-    <Typography variant="h2" gutterBottom>NEWS WEBSITE</Typography>
-    <div className={styles.container}>
-      {latestNews &&
-        latestNews.map((news) => (
-          <NewsCard
-            key={news.id}
-            author={news.by}
-            title={news.title}
-            date={news.time}
-            rating={news.score}
-          />
-        ))}
-    </div></>
+    <div className={styles.topContainer}>
+      <Typography variant="h2" gutterBottom>
+        NEWS WEBSITE
+      </Typography>
+      <Button variant="contained" size="large" onClick={() => setIsRefresh(!isRefresh)}>
+        UPDATE NEWS
+      </Button></div>
+      <div className={styles.container}>
+        {latestNews &&
+          latestNews.map((news) => (
+            <NewsCard
+              key={news.id}
+              author={news.by}
+              title={news.title}
+              date={news.time}
+              rating={news.score}
+            />
+          ))}
+      </div>
+    </>
   );
 }
