f598ccbd060c0726252bc33b01f760e410252930
Ирина Черняева <iridache@Irinas-MacBook-Air.local>
2023-03-09 20:53:42 +0300

    back btn and some style added

--- src/components/NewsItemPage.tsx
+++ src/components/NewsItemPage.tsx
@@ -1,6 +1,6 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { restApiService } from "../utils/RestApiService";
 import { NewsItem } from "../utils/types";
 import styles from "./NewsItemPage.module.css";
@@ -15,21 +15,24 @@ export function NewsItemPage() {
   });
 
   return (
-    <div className={styles.wrapper}>
-      <h2>{newsItem?.title}</h2>
-      <Typography variant="overline">
-        published{" "}
-        {newsItem?.time && new Date(+newsItem.time * 1000).toLocaleString()}
-      </Typography>
-      <Typography variant="overline" gutterBottom>
-        by {newsItem?.by}
-      </Typography>
-      <Typography variant="caption" gutterBottom>
-        source:<a href={newsItem?.url}> {newsItem?.url}</a>
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        {newsItem?.descendants} comments
-      </Typography>
-    </div>
+    <>
+      <Link to={'/'}><Button sx={{ mt: 3, ml: 5 }} variant="contained" size="large">
+        BACK
+      </Button></Link>
+      <div className={styles.wrapper}>
+        <h2 className={styles.heading}>{newsItem?.title}</h2>
+        <Typography className={styles.date} variant="overline">
+          published{" "}
+          {newsItem?.time && new Date(+newsItem.time * 1000).toLocaleString()}
+        </Typography>
+        <Typography variant="overline" gutterBottom>
+          by {newsItem?.by}
+        </Typography>
+        <Typography className={styles.source} variant="body2" gutterBottom>
+          source:<a href={newsItem?.url}> {newsItem?.url}</a>
+        </Typography>
+        <Button sx={{ mt: 3}}variant="outlined">{newsItem?.descendants} comments</Button>
+      </div>
+    </>
   );
 }
