d9b8d0072f393be67dce05b8b3fdc365b801ee4d
Ирина Черняева <iridache@Irinas-MacBook-Air.local>
2023-03-09 13:44:43 +0300

    api fixed

--- src/components/NewsCardList.tsx
+++ src/components/NewsCardList.tsx
@@ -1,46 +1,21 @@
 import { useEffect, useState } from "react";
+import { restApiService } from "../utils/RestApiService";
 import { NewsItem } from "../utils/types";
 import { NewsCard } from "./NewsCard";
 
 export function NewsCardList() {
-  const [newsIds, setNewsIds] = useState<number[]>();
   const [latestNews, setLatestNews] = useState<NewsItem[]>();
 
-  const getIds = async () => {
-    return await fetch(`https://hacker-news.firebaseio.com/v0/newstories.json`)
-      .then((res) => res.json())
-      .then((data: number[]) => data.filter((id: number, i: number) => i < 100))
-      .then((data) => setNewsIds(data))
-      .catch((e) => console.log(e));
-  };
-  //getIds()
-  let urls =
-    newsIds &&
-    newsIds?.map(
-      (id) => `https://hacker-news.firebaseio.com/v0/item/${id}.json`
-    );
-  let requests = urls && urls.map((url) => fetch(url));
-
-  requests &&
-    Promise.all(requests).then((responses) =>
-      Promise.all(responses.map((r) => r.json())).then((news) =>
-        setLatestNews(news)
-      )
-    );
-  console.log(newsIds);
-  console.log(urls);
-  console.log(requests);
-
-
-  // useEffect(() => {
-  //   getIds();
-  // }, []);
+  useEffect(() => {
+    restApiService.geNewsItems().then(data => setLatestNews(data))
+    }, [])
 
   return (
     <>
       {latestNews &&
         latestNews.map((news) => (
           <NewsCard
+            key={news.id}
             author={news.by}
             title={news.title}
             date={news.time}
--- src/utils/RestApiService.tsx
+++ src/utils/RestApiService.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { NewsItem } from "./types";
 
-export default function RestApiService() {
-  return (
-    <div>RestApiService</div>
-  )
+export class RestApiService {
+  fetchApi<T>(url: string, method: string = "GET", headers?: Object) {
+    const params = {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    return fetch(url, params).then((res) => {
+      if (res.status === 400) {
+        return Promise.reject(res.statusText);
+      }
+      return res.json();
+    });
+  }
+
+  geNewsItems = (): Promise<NewsItem[]> => {
+    return this.fetchApi(
+      `https://hacker-news.firebaseio.com/v0/newstories.json`
+    )
+      .then((data: number[]) => data.filter((id: number, i: number) => i < 100))
+      .then((ids) =>
+        ids.map((id) => `https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+      )
+      .then((urls) => urls.map((url) => this.fetchApi(url)))
+      .then((requests) => Promise.all(requests))
+      .then((responses) => Promise.all(responses));
+  };
 }
 
+export const restApiService = new RestApiService();
